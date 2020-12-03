f3527703dfb7552088ab591b3b010dda26c537f3
Aryan Kumar <aryan68125@gmail.com>
2020-12-03 18:00:49 +0530

    version 6 Delete Functionality Added Bug Fixes!

--- app/src/main/java/com/aditya/locationdemo/SavedLocationActivity3.java
+++ app/src/main/java/com/aditya/locationdemo/SavedLocationActivity3.java
@@ -1,8 +1,10 @@
 package com.aditya.locationdemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import android.widget.Toast;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class SavedLocationActivity3 extends AppCompatActivity {
 
@@ -29,13 +32,14 @@ public class SavedLocationActivity3 extends AppCompatActivity {
 
     //defining ArrayAdapter
     static ArrayAdapter arrayAdapter;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_location3);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.aditya.locationdemo", Context.MODE_PRIVATE);
+        sharedPreferences = this.getSharedPreferences("com.aditya.locationdemo", Context.MODE_PRIVATE);
 
         ArrayList<String> latitudes = new ArrayList<>(); //for storing latitudes
         ArrayList<String> longitudes = new ArrayList<>(); //for storing longitudes
@@ -95,5 +99,55 @@ public class SavedLocationActivity3 extends AppCompatActivity {
             }
         });
 
+        ListViewSavedLocation.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                //creating a new alert dialog box to ask the user weather they truely want to delete the note or not
+                new AlertDialog.Builder(SavedLocationActivity3.this).setIcon(R.drawable.danger).setTitle("Are you sure you want to delete the location?")
+                        .setMessage("The deleted locations cant be recovered!")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //here we will write the code to delete a note from the applications listView and from the memory
+                                //deleting the places and SavedPlaces from their respective Arraylist
+                                places.remove(position);
+                                Savedplaces.remove(position);
+                                //updating the ArrayAdapter
+                                arrayAdapter.notifyDataSetChanged();
+
+                                //now making changes to the shared Preferences when the notes are deleted
+                                //updating the notes shared preferences when we change anything in the notes Array
+                                ArrayList<String> places= new ArrayList<String>(SavedLocationActivity3.places);
+                                ArrayList<LatLng> Savedplaces = new ArrayList<LatLng>(SavedLocationActivity3.Savedplaces);
+
+                                try {
+
+                                    ArrayList<String> latitudes = new ArrayList<>();
+                                    ArrayList<String> longitudes = new ArrayList<>();
+
+            /*
+            coord.latitude = gives us latitude from the savedPlaces<latlang> ArrayList
+            coord.longitude = give us longitude from savedPlaces<latlang> ArrayList
+            SavedLocationActivity3.Savedplaces inorser to access this Array List it should be defined as static outside any functions
+             */
+                                    for (LatLng coord : SavedLocationActivity3.Savedplaces) {
+                                        latitudes.add(Double.toString(coord.latitude));
+                                        longitudes.add(Double.toString(coord.longitude));
+                                    }
+                                    //it will covert our arrayList into one big Massive String with all the entries
+                                    //sharedPreferences.edit().putString("ArrayToString",ObjectSerializer.serialize(concubine)).apply(); //for storing
+                                    sharedPreferences.edit().putString("places", ObjectSerializer.serialize(SavedLocationActivity3.places)).apply();
+                                    sharedPreferences.edit().putString("lats", ObjectSerializer.serialize(latitudes)).apply();
+                                    sharedPreferences.edit().putString("lons", ObjectSerializer.serialize(longitudes)).apply();
+
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).setNegativeButton("No",null).show();
+                return true;
+            }
+        });
     }
 }
